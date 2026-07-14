abc5557f471c48a62fb1f19bd47d08f0f641ebae
montatechbrasil <montatechbrasil@gmail.com>
2026-07-14 19:31:53 -0300

    Create lista.js

---
+++ blog/posts/lista.js
@@ -0,0 +1,62 @@
+var todosPosts = [
+    {
+        id: 6,
+        titulo: "Mudança Residencial: Como Proteger Seus Móveis",
+        categoria: "Mudanças",
+        resumo: "Checklist completo para desmontar, transportar e remontar seus móveis sem danos.",
+        imagem: "https://res.cloudinary.com/dxqm3lwrk/image/upload/v1774999808/montagem-moveis-cidades-entorno-02_vin2np.png",
+        data: "10 Jun 2026",
+        link: "posts/mudanca-desmontagem-moveis.html",
+        tempoLeitura: "7 min"
+    },
+    {
+        id: 5,
+        titulo: "Instalação de Painel de TV: Guia Completo",
+        categoria: "Dicas Práticas",
+        resumo: "Aprenda as melhores práticas para instalar seu painel de TV com segurança.",
+        imagem: "https://res.cloudinary.com/dxqm3lwrk/image/upload/v1774999808/montagem-profissional-valparaiso-06_ztmhmi.png",
+        data: "15 Jun 2026",
+        link: "posts/painel-tv-instalacao.html",
+        tempoLeitura: "5 min"
+    },
+    {
+        id: 4,
+        titulo: "Como Fazer Seu Guarda-Roupa Durar Mais Tempo",
+        categoria: "Manutenção",
+        resumo: "Dicas práticas de manutenção para aumentar a vida útil do seu guarda-roupa.",
+        imagem: "https://res.cloudinary.com/dxqm3lwrk/image/upload/v1774999809/montagem-moveis-valparaiso-goias-01_izqrzz.png",
+        data: "20 Jun 2026",
+        link: "posts/guardaroupa-manutencao.html",
+        tempoLeitura: "4 min"
+    },
+    {
+        id: 3,
+        titulo: "Móveis Modulados: Vantagens e Desvantagens",
+        categoria: "Móveis Modulados",
+        resumo: "Entenda se os móveis modulados são a melhor opção para o seu projeto.",
+        imagem: "https://res.cloudinary.com/dxqm3lwrk/image/upload/v1774999808/montagem-moveis-cidades-entorno-02_vin2np.png",
+        data: "28 Jun 2026",
+        link: "posts/moveis-modulados-vantagens.html",
+        tempoLeitura: "6 min"
+    },
+    {
+        id: 2,
+        titulo: "Como Escolher um Bom Montador de Móveis em Valparaíso",
+        categoria: "Dicas de Contratação",
+        resumo: "Saiba o que avaliar antes de contratar um profissional para montar seus móveis.",
+        imagem: "https://res.cloudinary.com/dxqm3lwrk/image/upload/v1774999808/montagem-profissional-valparaiso-06_ztmhmi.png",
+        data: "05 Jul 2026",
+        link: "posts/como-escolher-montador.html",
+        tempoLeitura: "4 min"
+    },
+    {
+        id: 1,
+        titulo: "5 Dicas Para Sua Cozinha Planejada Ficar Perfeita",
+        categoria: "Cozinhas Planejadas",
+        resumo: "Descubra os segredos para um projeto de cozinha planejada que alie beleza e funcionalidade.",
+        imagem: "https://res.cloudinary.com/dxqm3lwrk/image/upload/v1774999809/montagem-moveis-valparaiso-goias-01_izqrzz.png",
+        data: "10 Jul 2026",
+        link: "posts/cozinha-planejada-dicas.html",
+        tempoLeitura: "5 min"
+    }
+];
