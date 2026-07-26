a69662393c948239edf5178824c880e37765afa6
montatechbrasil <montatechbrasil@gmail.com>
2026-07-26 15:17:56 -0300

    Update lista.js

--- blog/posts/lista.js
+++ blog/posts/lista.js
@@ -1,5 +1,5 @@
 var todosPosts = [
-    //
+    // 🆕 ID 1 - PRIMOGÊNITO (26 Jul)
     {
         id: 1,
         titulo: "Quanto Custa um Montador de Móveis em Valparaíso? Guia de Preços 2026",
@@ -10,7 +10,20 @@ var todosPosts = [
         link: "posts/quanto-custa-montador-moveis-valparaiso.html",
         tempoLeitura: "7 min"
     },
-    // Posts existentes abaixo...
+    // ID 2 - SEGUNDO (14 Jul)
+    {
+        id: 2,
+        titulo: "Guia Completo de Parafusos para Montagem de Móveis: Tipos, Ferramentas e Segredos",
+        categoria: "Dicas Práticas",
+        resumo: "Descubra os tipos de parafusos usados na montagem de móveis, ferramentas essenciais e dicas profissionais. Guia definitivo!",
+        imagem: "https://res.cloudinary.com/dxqm3lwrk/image/upload/v1774999808/montagem-profissional-valparaiso-06_ztmhmi.png",
+        data: "14 Jul 2026",
+        link: "posts/parafusos-montagem-moveis.html",
+        tempoLeitura: "8 min"
+    },
+    // Posts antigos abaixo (IDs 6 a 1 antigos)
+    // ...
+];
     
     {
     id: 99,
