6d3e355f4d43bded185a3783aeaa1b395ed5ff7c
montatechbrasil <montatechbrasil@gmail.com>
2026-08-20 14:10:17 -0300

    Create materiais.js

---
+++ calculadora/js/materiais.js
@@ -0,0 +1,47 @@
+{
+  "pequeno": {
+    "nome": "Embalagem Básica",
+    "descricao": "Ideal para móveis pequenos: cadeiras, sapateiras, cabeceiras",
+    "faixaMin": 20,
+    "faixaMax": 40,
+    "materiais": [
+      "Filme Stretch (1/2 rolo)",
+      "Plástico-bolha (1/2 rolo)",
+      "Fita Adesiva Larga (1/4 rolo)",
+      "Sacos Ziploc (2 unidades)"
+    ]
+  },
+  "medio": {
+    "nome": "Embalagem Intermediária",
+    "descricao": "Ideal para móveis médios: cômodas, racks, painéis, berços",
+    "faixaMin": 40,
+    "faixaMax": 80,
+    "materiais": [
+      "Filme Stretch (1 rolo)",
+      "Plástico-bolha (1/2 rolo)",
+      "Papelão Ondulado (1/2 rolo)",
+      "Cantoneiras (4 unidades)",
+      "Fita Adesiva Larga (1/2 rolo)",
+      "Sacos Ziploc (4 unidades)",
+      "Etiquetas Adesivas"
+    ]
+  },
+  "grande": {
+    "nome": "Embalagem Premium",
+    "descricao": "Ideal para móveis grandes: guarda-roupas, cozinhas, cristaleiras",
+    "faixaMin": 80,
+    "faixaMax": 180,
+    "materiais": [
+      "Filme Stretch (2 rolos)",
+      "Plástico-bolha (1 rolo)",
+      "Papelão Ondulado (1 rolo)",
+      "Cantoneiras (8 unidades)",
+      "Espuma em Rolo (1 rolo)",
+      "Mantas de Transporte (2-3 unidades)",
+      "Fita Adesiva Larga (1 rolo)",
+      "Fita de Arquear",
+      "Sacos Ziploc (6 unidades)",
+      "Etiquetas Adesivas"
+    ]
+  }
+}
