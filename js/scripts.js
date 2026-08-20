0bcc13a60e3d01733963a96d07392d7f7dbb326d
montatechbrasil <montatechbrasil@gmail.com>
2026-08-20 18:42:29 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -69,7 +69,7 @@
         const galleryImages = [
             'https://res.cloudinary.com/dxqm3lwrk/image/upload/v1785594410/painel-tv-instalado_led_valparaiso-go_chdgu7.jpg',
             'https://res.cloudinary.com/dxqm3lwrk/image/upload/v1785594408/montagem-guarda-roupas-planejado_tgiaxw.jpg',
-            'https://res.cloudinary.com/dxqm3lwrk/image/upload/v1785594407/montagem_guarda_roupas_valparaiso-ocidental_nvowi1.jpg',
+            'https://res.cloudinary.com/dxqm3lwrk/image/upload/v1787261983/cozinhas_planejadas_para_apartamentos_btu4rf.jpg',
             'https://res.cloudinary.com/dxqm3lwrk/image/upload/v1785594407/montagem-cozinha-aereos-valparaiso-go_qqmnq9.jpg',
             'https://res.cloudinary.com/dxqm3lwrk/image/upload/v1785594412/cozinha-planejada-montada-valparaiso_befqgr.jpg',
             'https://res.cloudinary.com/dxqm3lwrk/image/upload/v1785594413/desmontagem-mudanca-cidade-ocidental_afxyre.jpg',
